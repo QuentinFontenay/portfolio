5c3464a15d18b042126be38304bacc8708357263
Quentin Fontenay <quentin.fontenay@hotmail.com>
2021-08-17 22:57:05 +0200

    modif experience

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -10,17 +10,17 @@ export const Experience = ({ experience, education }) => {
             <SectionTitle main>Mes experiences</SectionTitle>
             <Container>
                 <Card className="experience">
-                    <Text size="1.5rem" weight={600} margin="0 0 1rem 0">
+                    <Text size="3rem" weight={600} margin="0 0 1rem 0">
                         Experience Professionel
                     </Text>
                     {experience.map((item, key) => (
                         <CardDescription key={key}>
-                            <Text size="1rem" weight={600} padding="1rem 0 0 0">
+                            <Text size="2rem" weight={600} padding="1rem 0 0 0">
                                 {item.role}
                             </Text>
 
-                            <Text size="0.8rem">
-                                <FaCalendarAlt width={12} style={{ margin: '0 0.5rem 0.2rem 0' }} />
+                            <Text size="1rem">
+                                <FaCalendarAlt size="2rem" style={{ margin: '0 0.5rem 0.2rem 0' }} />
                                 {item.period} | {item.company}
                             </Text>
                             <Text padding="0.5rem 0" weight={300}>
@@ -30,7 +30,7 @@ export const Experience = ({ experience, education }) => {
                     ))}
                 </Card>
                 <Card className="education">
-                    <Text size="1.5rem" weight={600} margin="0 0 1rem 0">
+                    <Text size="3rem" weight={600} margin="0 0 1rem 0">
                         Parcours scolaire
                     </Text>
                     {education.map((item, key) => (
@@ -40,7 +40,7 @@ export const Experience = ({ experience, education }) => {
                             </Text>
 
                             <Text size="0.8rem">
-                                <FaCalendarAlt width={12} style={{ margin: '0 0.5rem 0.2rem 0' }} />
+                                <FaCalendarAlt size="2rem" style={{ margin: '0 0.5rem 0.2rem 0' }} />
                                 {item.period} | {item.company}
                             </Text>
                             <Text padding="0.5rem 0" weight={300}>
