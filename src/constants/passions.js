6e0f047f9622800b6e3a410507eded547d152650
Quentin Fontenay <quentin.fontenay@hotmail.com>
2021-09-25 22:51:33 +0200

    ajout react import

--- src/constants/passions.js
+++ src/constants/passions.js
@@ -1,6 +1,7 @@
 import { BiTennisBall } from 'react-icons/bi';
 import { GiCommercialAirplane } from 'react-icons/gi';
 import { BsNewspaper } from 'react-icons/bs';
+import React from 'react';
 
 export const passions = [
     { icon: <BiTennisBall fontSize="5rem" />, text: 'Tennis'},
