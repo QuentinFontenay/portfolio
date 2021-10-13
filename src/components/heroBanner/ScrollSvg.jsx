0b6c46557e88dd771e6ceed42630efd304233c0b
Quentin Fontenay <quentin.fontenay@hotmail.com>
2021-10-13 22:06:06 +0200

    reduction delai loading + modif couleur scroll svg

--- src/components/heroBanner/ScrollSvg.jsx
+++ src/components/heroBanner/ScrollSvg.jsx
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import ThemeContext from '../../context/ThemeContext';
 
-const Scroll = ({ theme }) => {
+const Scroll = () => {
+    const theme = useContext(ThemeContext);
+
     return (
         <div
             style={{
@@ -10,9 +13,9 @@ const Scroll = ({ theme }) => {
             }}>
             <svg
                 width="40px" viewBox="0 0 14.334 24.75" >
-                <circle className="circle-1" fill="white" cx="7.167" cy="6" r="1.2" />
-                <circle className="circle-2" fill="white" cx="7.167" cy="6" r="1.2" />
-                <path stroke="white" fill="transparent" d="M7.167,0.5C3.485,0.5,0.5,3.485,0.5,7.167v10.416c0,3.682,2.985,6.667,6.667,6.667s6.667-2.985,6.667-6.667V7.167C13.834,3.485,10.849,0.5,7.167,0.5z" />
+                <circle className="circle-1" fill={theme.mode == 'dark' ? 'white' : 'black'} cx="7.167" cy="6" r="1.2" />
+                <circle className="circle-2" fill={theme.mode == 'dark' ? 'white' : 'black'} cx="7.167" cy="6" r="1.2" />
+                <path stroke={theme.mode == 'dark' ? 'white' : 'black'} fill="transparent" d="M7.167,0.5C3.485,0.5,0.5,3.485,0.5,7.167v10.416c0,3.682,2.985,6.667,6.667,6.667s6.667-2.985,6.667-6.667V7.167C13.834,3.485,10.849,0.5,7.167,0.5z" />
             </svg>
         </div>
     );
