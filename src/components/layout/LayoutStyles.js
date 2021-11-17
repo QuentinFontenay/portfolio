8445d27f1ec6fd3c0338b6fe499daaea74f8e74e
Quentin Fontenay <quentin.fontenay@hotmail.com>
2021-11-17 22:24:20 +0100

    suppresion element inutile

--- src/components/layout/LayoutStyles.js
+++ src/components/layout/LayoutStyles.js
@@ -5,18 +5,3 @@ export const Container = styled.div`
     width: 100%;
     margin: auto;
 `;
-
-export const Copyright = styled.span`
-  grid-row: 2;
-  grid-column: 1/3;
-  text-align: center;
-  padding: 1em 0;
-  width: 100%;
-  border-top: 1px solid #242424;
-  font-size: 0.9em;
-  max-width: unset;
-  @media(max-width: 600px) {
-    grid-row: 3;
-    grid-column: 1;
-  }
-`
